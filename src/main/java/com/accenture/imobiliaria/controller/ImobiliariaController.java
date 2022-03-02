6a8c4f2fcb808a161b3ae678235b1224027f5256
btoledo90 <brunotq19@hotmail.com>
2022-03-02 18:11:30 -0300

    ImobiliariaService

--- src/main/java/com/accenture/imobiliaria/controller/ImobiliariaController.java
+++ src/main/java/com/accenture/imobiliaria/controller/ImobiliariaController.java
@@ -1,5 +1,6 @@
 package com.accenture.imobiliaria.controller;
 
+import com.accenture.imobiliaria.dto.ImobiliariaDTO;
 import com.accenture.imobiliaria.entities.Imobiliaria;
 import com.accenture.imobiliaria.services.ImobiliariaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,18 +21,24 @@ public class ImobiliariaController {
     @Autowired
     private ImobiliariaService imobiliariaService;
 
-
     @PostMapping
     //@Consumes(MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> submit(
+    public ResponseEntity<ImobiliariaDTO> insert(
 
             @RequestParam("msgTexto") String mensagem,
             @RequestParam("imagem") MultipartFile multipartFile) {
-
-        return ResponseEntity.ok(mensagem);
+            ImobiliariaDTO dto = new ImobiliariaDTO(null, mensagem);
+            dto = imobiliariaService.service(dto);
+            return ResponseEntity.ok(dto);
     }
 
-
+//    @PostMapping
+//    public ResponseEntity<ImobiliariaDTO> insert(@RequestBody ImobiliariaDTO dto) {
+//        dto = service.insert(dto);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+//                .buildAndExpand(dto.getId()).toUri();
+//        return ResponseEntity.created(uri).body(dto);
+//    }
 
 }
 
